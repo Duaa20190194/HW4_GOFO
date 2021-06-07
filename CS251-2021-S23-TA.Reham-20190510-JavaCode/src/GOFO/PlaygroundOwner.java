e4401e4f41b3d7f0b675b5da9d6e9795d7a84285
MarwanAshraf-20190510 <62058920+Marwan-Ashraf@users.noreply.github.com>
2021-06-08 00:50:25 +0200

    Update PlaygroundOwner.java

--- CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/PlaygroundOwner.java
+++ CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/PlaygroundOwner.java
@@ -8,6 +8,15 @@ public class PlaygroundOwner {
 	private String Location;
 	private String Password;
 	
+	/**
+	 * This is a function that registers the information of a playground owner
+	 * @param Name
+	 * @param ID
+	 * @param Password
+	 * @param Email
+	 * @param Phone
+	 * @param Location
+	 */
 	public void Register(String Name, int ID, String Password, String Email, String Phone , String Location) {
 		this.Name=Name;
 		this.ID=ID;
@@ -16,6 +25,9 @@ public class PlaygroundOwner {
 		this.Phone=Phone;
 		this.Location=Location;
 		}
+	/**
+	 * This is a function that displays the information stored of a playground owner
+	 */
 	public void display() {
 		System.out.println("your informations is: name: " + Name + " ID: " + ID + " email: "+Email + " phone: " + Phone + " location: " + Location);
 	}

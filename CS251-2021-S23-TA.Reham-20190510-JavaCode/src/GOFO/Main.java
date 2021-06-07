11757b5f24a534efa4b3e1f7717b18edcd70bd3e
MarwanAshraf-20190510 <62058920+Marwan-Ashraf@users.noreply.github.com>
2021-06-07 20:31:54 +0200

    Update Main.java

--- CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/Main.java
+++ CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/Main.java
@@ -12,19 +12,37 @@ public class Main {
 			PlayGround pg = new PlayGround();
 			system system=new system();
 			
+			System.out.println("enter your name");
 			String Name= scan.nextLine();
+			System.out.println("enter your ID");
 			int ID= scan.nextInt(); 
+			System.out.println("enter your password");
 			String Password= scan.nextLine();
-			String Email= scan.nextLine(); 
-			String Phone= scan.nextLine(); 
-			String Location;
-			Location = scan.nextLine();
+			System.out.println("enter your email");
+			String Email= scan.nextLine();
+			System.out.println("enter your phone");
+			String Phone= scan.nextLine();
+			System.out.println("enter your location");
+			String Location = scan.nextLine();
 			
 			PO.Register(Name,ID,Password,Email,Phone ,Location);
 			system.CreateRegistration(PO);
 			
 			PO.display();
+			System.out.println("enter playgrounds name");
+			String name=scan.nextLine();
+			System.out.println("enter playgrounds location");
+			String location=scan.nextLine();
+			System.out.println("enter playgrounds size");
+			float size=scan.nextFloat();
+			System.out.println("enter playgrounds available hours");
+			int availableHours=scan.nextInt();
+			System.out.println("enter the price per hour");
+			float pricePerHour=scan.nextFloat();
+			System.out.println("enter the cancellation period");
+			String cancellationPeriod=scan.nextLine();
 			
+			pg.AddPlaygroundInfo(name, location, size, availableHours, pricePerHour, cancellationPeriod);
 		}
 	}
 	

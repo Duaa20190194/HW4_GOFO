4e6e7bb1e53d2b91bbfffa4cd73c0cc70118ab4c
MarwanAshraf-20190510 <62058920+Marwan-Ashraf@users.noreply.github.com>
2021-06-08 00:48:36 +0200

    Update system.java

--- CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/system.java
+++ CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/system.java
@@ -2,6 +2,10 @@ package GOFO;
 
 public class system {
 	private PlaygroundOwner owner;
+	/**
+	 * This is a function that stores the information of a playground owner
+	 * @param owner
+	 */
 	public void CreateRegistration(PlaygroundOwner owner) {
 		this.owner=owner;
 	}

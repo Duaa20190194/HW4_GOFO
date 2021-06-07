3ed31ff8600c550d2952d99b22f41e0d7a1e9107
MarwanAshraf-20190510 <62058920+Marwan-Ashraf@users.noreply.github.com>
2021-06-07 18:23:23 +0200

    Update PlaygroundOwner.java

--- CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/PlaygroundOwner.java
+++ CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/PlaygroundOwner.java
@@ -1,5 +1,20 @@
 package GOFO;
 
 public class PlaygroundOwner {
-
+  private String Name;
+	private int ID;
+	private String Email;
+	private String Phone;
+	private String Location;
+	private String Password;
+	
+	public void Register(String Name, int ID, String Password, String Email, String Phone , String Location) {
+		this.Name=Name;
+		this.ID=ID;
+		this.Password=Password;
+		this.Email=Email;
+		this.Phone=Phone;
+		this.Location=Location;
+		}
+	
 }

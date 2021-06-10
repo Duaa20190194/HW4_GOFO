2ea1df09742c727d87a1f329197da1fe4ac41a62
Duaa20190194 <85021586+Duaa20190194@users.noreply.github.com>
2021-06-10 09:12:39 +0200

    Update Booking.java

--- CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/Booking.java
+++ CS251-2021-S23-TA.Reham-20190510-JavaCode/src/GOFO/Booking.java
@@ -6,16 +6,25 @@ public class Booking {
     
     private int numofBooking;
     private int timeSlot;
+    private Playground name; 
     
     public void BookTimeSlot(int timeslot, int num){
     this.timeSlot = timeslot;
-    this.numofBooking = num;
+    this.numofBooking = num;   
     }
     
     public void display(){
         System.out.println("your Booking number is " + numofBooking + "  your time slot :" + timeSlot);
     
     }
+    
+    public void selectPlayground(Playground name){
+        this.name = name;   
+        }
+        
+        public void displayPlaygroundInfo(){
+        name.display();
+        }
     
 }
 
